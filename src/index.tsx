4a728b851a359498b5b5a586b616cd7c61a2c423
Dr B. <drmurolinq@gmail.com>
2025-05-31 23:25:33 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
-// Placeholder for IPSSQuestionnaire page
-export {};
-// React app bootstrap placeholder
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
