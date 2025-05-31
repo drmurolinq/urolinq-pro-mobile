6114385149ea591ad88d87b40c991df710aa9765
Dr B. <drmurolinq@gmail.com>
2025-05-31 15:28:12 +0200

    Create index.tsx

---
+++ src/navigation/index.tsx
@@ -0,0 +1,8 @@
+import EnuresisQuestionnaire from '../pages/EnuresisQuestionnaire';
+
+// Inside your Stack.Navigator:
+<Stack.Screen
+  name="EnuresisQuestionnaire"
+  component={EnuresisQuestionnaire}
+  options={{ title: 'Bedwetting Survey' }}
+/>
