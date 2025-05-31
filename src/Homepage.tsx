1348ec66d2692c92e5b0c600f00082a412076fa1
Dr B. <drmurolinq@gmail.com>
2025-05-31 23:18:07 +0200

    Create Homepage.tsx

---
+++ src/Homepage.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      <h1 className="text-2xl font-bold">Questionnaires</h1>
+      <div className="flex flex-col gap-2">
+        <Button asChild>
+          <Link to="/ipss">IPSS Questionnaire</Link>
+        </Button>
+        <Button asChild>
+          <Link to="/mipro">MIPRO Questionnaire</Link>
+        </Button>
+        <Button asChild>
+          <Link to="/enuresis">Enuresis Questionnaire</Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
